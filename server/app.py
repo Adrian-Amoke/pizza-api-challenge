176ccb3c3239d5d7a9c529137a487be0c4ec7952
Adrian-Amoke <adrian.amoke@student.moringaschool.com>
2025-06-23 09:43:24 +0300

    Added pipfile

--- server/app.py
+++ server/app.py
@@ -4,11 +4,11 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from dotenv import load_dotenv
 import os
-from server.controllers.pizza_controller import pizza_bp
-from server.controllers.index_controller import index_bp
-from server.controllers.restaurant_controller import restaurant_bp
-from server.controllers.restaurant_pizza_controller import restaurantpizza_bp
-from server.config import db
+from controllers.pizza_controller import pizza_bp
+from controllers.index_controller import index_bp
+from controllers.restaurant_controller import restaurant_bp
+from controllers.restaurant_pizza_controller import restaurantpizza_bp
+from config import db
 
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s"
@@ -24,7 +24,7 @@ load_dotenv()
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("FLASK_SQLALCHEMY_DATABASE_URI")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-import server.models
+import models
 
 app.register_blueprint(pizza_bp)
 app.register_blueprint(index_bp)
